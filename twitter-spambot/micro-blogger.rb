4b16b8a2db599ae331eb93cc514af9b14c268f1c
Tyler Guillen <tyguillen@gmail.com>
2015-09-04 18:28:13 -0700

    add basic tweet interface

--- twitter-spambot/micro-blogger.rb
+++ twitter-spambot/micro-blogger.rb
@@ -1,5 +1,46 @@
-require 'jumpstart-auth'
+require 'jumpstart_auth'
 
 class MicroBlogger
-  
+  attr_reader :client
+
+  def initialize
+  	puts "Initializing..."
+  	@client = JumpstartAuth.twitter
+  end
+
+  def tweet(message)
+  	if message.length <= 140
+  		@client.update(message)
+  	else
+  		puts "Your tweet is too long!"
+  	end
+  end
+
+  def run
+  	puts "Welcome to the JSL Twitter Client!"
+
+  	command = ""
+  	while command != "q"
+  		printf "enter command: "
+  		input = gets.chomp
+  		parts = input.split(" ")
+  		command = parts[0]
+
+  		case command
+  		when "q" then puts "Goodbye!"
+  		when "t" then tweet(parts[1..-1].join(" "))
+		  when 'dm' then dm(parts[1], parts[2..-1].join(" "))
+  		else
+  			puts "Sorry, I don't know how to #{command}"
+  		end
+  	end
+  end
+
+  def dem(target, message)
+  	puts "Trying to send #{target} this direct message:"
+  	puts message
+  end
 end
+
+blogger = MicroBlogger.new
+blogger.run
